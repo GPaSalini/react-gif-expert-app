d1dadeed75cb753be308682312ef7ba8d9775d28
Giordano Paolo Salini Irribarra <giordano.salini1801@alumnos.ubiobio.cl>
2022-11-24 18:34:15 -0300

    primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,26 @@
+import {useState} from 'react'
+import {AddCategory,GifGrid} from './components'
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch'])
+
+    const onAddCategory= (newCategory) => {
+        if (categories.includes(newCategory)) return
+        setCategories([newCategory,...categories])
+    }
+
+    return(
+        <>
+            <h1>Gif Expert App</h1>
+
+            <AddCategory onNewCategory={onAddCategory} />   {/* esto realiza el cambio en este componente padre*/}
+
+            {
+                categories.map((category) => (
+                    <GifGrid key={category} category={category}/>
+                ))
+            }
+        </>
+    )
+}
